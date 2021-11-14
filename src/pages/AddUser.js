9816921832e583ae0e96c22bc6b8ff4232c10eac
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-14 06:52:08 +0300

    Add New User

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -6,56 +6,75 @@ import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
+  const [error, setError] = useState("");
   let navigate = useNavigate();
-  const [user, setUser] = useState({
+
+  const [state, setState] = useState({
     name: "",
     email: "",
     address: "",
     contact: "",
   });
 
-  const { name, email, address, contact } = user;
+  const { name, email, address, contact } = state;
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    console.log(e.target.value);
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    if (!name || !email || !address || !contact) {
+      setError("Please enter value in all Fields");
+    }
+  };
   return (
     <div className="container">
       <div className="container-row">
-        <h2>React CRUD App</h2>
         <h3>Add New User</h3>
         <Box
           className="form-input"
           component="form"
           sx={{
-            "& > :not(style)": { m: 1, width: "45ch" },
+            "& > :not(style)": { m: 1, width: "55ch" },
           }}
           noValidate
           autoComplete="off"
+          onSubmit={handleSubmit}
         >
           <TextField
             id="standard-basic"
             label="Name"
             variant="standard"
-            value={name}
+            defaultValue={name}
             type="text"
+            onChange={handleInputChange}
           />
           <TextField
             id="standard-basic"
             label="Email"
             variant="standard"
-            value={email}
-            type="text"
+            defaultValue={email}
+            type="email"
+            onChange={handleInputChange}
           />
           <TextField
             id="standard-basic"
             label="Address"
             variant="standard"
-            value={address}
+            defaultValue={address}
             type="text"
+            onChange={handleInputChange}
           />
           <TextField
             id="standard-basic"
             label="Contact"
             variant="standard"
-            value={contact}
-            type="text"
+            defaultValue={contact}
+            type="number"
+            onChange={handleInputChange}
           />
           <div className="submit-button">
             <Button
